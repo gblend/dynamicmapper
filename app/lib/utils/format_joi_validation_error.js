054f48e5407796edc605144de3284295e1b99788
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-02 11:07:29 +0100

    Add joi validation error formatter

---
+++ app/lib/utils/format_joi_validation_error.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports.formatValidationError = ({ details }) => {
+    return details.map(detail => {
+        if (detail.message) {
+            return detail.message.split('\"').join('')
+        }
+        return detail.message;
+    });
+}
