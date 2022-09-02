cc41f1ec1ebd4ea8a2c502e08acf351950890f1b
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-02 11:21:15 +0100

    Implement handler for loading provider data

--- app/controllers/providerController.js
+++ app/controllers/providerController.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const {BadRequestError} = require('../lib/errors');
-const {specificationValidator} = require('../lib/validator')
+const {BadRequestError, NotFoundError} = require('../lib/errors');
+const {providerDataValidator, specificationValidator} = require('../lib/validator')
 const {
 	formatValidationError,
 	adaptRequest,
 	logger,
 	StatusCodes,
+	prepareLoadData,
 } = require('../lib/utils');
 const {DataSpecification} = require('../models/ProviderSpecification');
+const {ProviderData} = require('../models/ProviderData');
 
 const createSpecification = async (req, res) => {
 	const {body, method, path} = adaptRequest(req);
@@ -43,6 +45,47 @@ const createSpecification = async (req, res) => {
 	});
 }
 
+const loadData = async (req, res) => {
+	const {body: payload, path, method} = adaptRequest(req);
+	const providerId = payload.providerId;
+
+	if (!providerId) {
+		throw new BadRequestError('Invalid provider id');
+	}
+
+	const providerSpecification = await DataSpecification.findOne({providerId});
+	if (!providerSpecification) {
+		logger.info(`Provider specification does not exist - ${method} - ${path}`);
+		throw new NotFoundError('Provider specification does not exist');
+	}
+
+	if (!providerSpecification.fields.length) {
+		throw new BadRequestError('Invalid provider specification fields. Please update fields specification');
+	}
+	const {error} = providerDataValidator.validateDto(payload, providerSpecification.fields);
+	if (error) {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			data: {errors: formatValidationError(error)}
+		});
+	}
+
+	const dataToLoad = prepareLoadData(payload)
+	const providerData = await ProviderData.insertMany(dataToLoad);
+	if (!providerData) {
+		logger.error(`Provider data loading failed, providerId: ${providerId} - ${method} - ${path}`);
+		throw new BadRequestError('Provider data loading failed');
+	}
+
+	logger.info(`Successfully loaded data for provider: ${providerId}`)
+	return res.status(StatusCodes.OK).json({
+		message: 'Successfully loaded data for provider',
+		data: {
+			providerData
+		}
+	});
+}
+
 module.exports = {
+	loadData,
 	createSpecification
 }
