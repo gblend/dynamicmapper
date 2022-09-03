c1dace21ff4c6f87eed1fedbd38964db4c122bc4
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-03 11:27:00 +0100

    Include logger util

--- app/lib/utils/index.js
+++ app/lib/utils/index.js
@@ -2,8 +2,10 @@
 
 const {formatValidationError} = require('./format_joi_validation_error');
 const {adaptRequest} = require('./adapt_request');
+const {logger} = require('./logger');
 
 module.exports = {
 	formatValidationError,
-	adaptRequest
+	adaptRequest,
+	logger
 }
