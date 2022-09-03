ffb3968a2b056083c9e721de59ba68657771b8f6
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-03 11:31:16 +0100

    Add data provider data validators

--- app/lib/validator.js
+++ app/lib/validator.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const {allowedFieldTypesMapper} = require('../lib/utils');
 
 const specificationValidator = {
 	validateDto: (payload) => {
@@ -11,6 +12,29 @@ const specificationValidator = {
 	}
 }
 
+const providerDataValidator = {
+	validateDto: (payload, fields) => {
+		const allowedDataSchema = allowedFieldTypesMapper({fields});
+
+		// validate payload data items key length against data specification length for the given provider id length
+		const allowedDataKeyLengthSchema = Joi.array().items(Joi.object().keys().length(Object.keys(allowedDataSchema).length));
+		const isLengthMismatchError = allowedDataKeyLengthSchema.validate(payload.data);
+
+		if (!isLengthMismatchError.error) {
+			const validSchema = Joi.object({
+				providerId: Joi.number().required(),
+				data: Joi.array().items(allowedDataSchema).min(1)
+			});
+
+			// validate data items keys and data types against data specification for the given provider id.
+			return validSchema.validate(payload);
+		}
+
+		return isLengthMismatchError;
+	}
+}
+
 module.exports = {
 	specificationValidator,
+	providerDataValidator
 }
