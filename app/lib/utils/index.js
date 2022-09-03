87a19fa0709cb1c9149b2516fac74533db8d0ac1
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-03 11:27:38 +0100

    Include app status generator

--- app/lib/utils/index.js
+++ app/lib/utils/index.js
@@ -3,9 +3,11 @@
 const {formatValidationError} = require('./format_joi_validation_error');
 const {adaptRequest} = require('./adapt_request');
 const {logger} = require('./logger');
+const {appStatus} = require('./app_status');
 
 module.exports = {
 	formatValidationError,
 	adaptRequest,
-	logger
+	logger,
+	appStatus
 }
