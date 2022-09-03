62e3516425fc195d9c6bd5e64c5324ebfbbb69b9
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-03 11:26:33 +0100

    Include request adaptor

--- app/lib/utils/index.js
+++ app/lib/utils/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const {formatValidationError} = require('./format_joi_validation_error');
+const {adaptRequest} = require('./adapt_request');
 
 module.exports = {
-	formatValidationError
+	formatValidationError,
+	adaptRequest
 }
