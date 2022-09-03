c03db4daf446516cdab5b887448ed45aea6c10db
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-03 11:28:34 +0100

    Include data mappers

--- app/lib/utils/index.js
+++ app/lib/utils/index.js
@@ -4,12 +4,18 @@ const {formatValidationError} = require('./format_joi_validation_error');
 const {adaptRequest} = require('./adapt_request');
 const {logger} = require('./logger');
 const {appStatus} = require('./app_status');
+const {mapQueryFilters} = require('./query_filter_mapper');
 const StatusCodes = () => require('http-status-codes').StatusCodes;
+const {allowedFieldTypesMapper} = require('./field_types_mapper');
+const {prepareLoadData} = require('../utils/prepare_provider_data');
 
 module.exports = {
 	formatValidationError,
 	adaptRequest,
 	logger,
 	appStatus,
-	StatusCodes: StatusCodes()
+	StatusCodes: StatusCodes(),
+	mapQueryFilters,
+	allowedFieldTypesMapper,
+	prepareLoadData
 }
