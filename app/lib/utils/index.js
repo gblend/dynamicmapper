3113fd03e4244afe6650b514aba0914be61b7b32
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-09-03 11:28:07 +0100

    Include http status codes

--- app/lib/utils/index.js
+++ app/lib/utils/index.js
@@ -4,10 +4,12 @@ const {formatValidationError} = require('./format_joi_validation_error');
 const {adaptRequest} = require('./adapt_request');
 const {logger} = require('./logger');
 const {appStatus} = require('./app_status');
+const StatusCodes = () => require('http-status-codes').StatusCodes;
 
 module.exports = {
 	formatValidationError,
 	adaptRequest,
 	logger,
-	appStatus
+	appStatus,
+	StatusCodes: StatusCodes()
 }
